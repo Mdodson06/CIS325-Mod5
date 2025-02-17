14fdca31b1d5be5d17632493118ceee3139596dd
Mdodson06 <mariadodson8@gmail.com>
2025-02-17 13:28:44 -0600

    Add comments, remove redundant code

--- server.js
+++ server.js
@@ -9,21 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
 
-    // Hardcoded user credentials
-    const validUsername = 'admin';
-    const validPassword = '12345';
-
-    // Validate credentials
-    if (username === validUsername && password === validPassword) {
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
-});
-
+/*when on login page, upon being given a submission, take the username and password 
+and compare it to the hardcoded values
+If the username and password provided are valid, return with the res json having the value 
+success assigned true. If not, success is false.
+*/
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -39,6 +30,7 @@ app.post('/login', (req, res) => {
     }
   });
 
+  //Default page so user knows that they need to navigate to login.html
   app.get('/', (req, res) => {
     res.send('Navigate to login page');
   });
